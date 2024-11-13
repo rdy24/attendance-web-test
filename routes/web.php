3e47e44cd5d3f06cca40ef9c217ea39a61cb2652
Raflizar Deswandi Yahya <raflizar.sekawanmedia@gmail.com>
2024-11-13 21:27:08 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MineController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+})->middleware('auth');
+
+Route::get('/login', [AuthController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate')->middleware('guest');
+Route::post('/logout', [AuthController::class, 'logout']);
